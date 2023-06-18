3869f73c4277b66fc47f59e7efab9c1ff13073a4
JinXin <aihenry2980@gmail.com>
2023-06-19 00:04:16 +0900

    first commit

---
+++ app/src/main/java/com/example/learnrecycleview/ItemsViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.learnrecycleview
+
+data class ItemsViewModel(val image: Int, val text: String) {
+
+
+}
---
+++ app/src/main/java/com/example/learnrecycleview/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.learnrecycleview
+
+import android.os.Bundle
+import android.widget.Button
+//import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learnrecycleview.ui.theme.LearnRecycleviewTheme
+
+import org.jsoup.HttpStatusException
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		setContentView(R.layout.activity_main)
+
+		// getting the recyclerview by its id
+		val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+
+		// this creates a vertical layout Manager
+		recyclerview.layoutManager = LinearLayoutManager(this)
+
+		val button = findViewById<Button>(R.id.btn_refresh)
+		button.setOnClickListener {
+			getHtmlFromWeb()
+		}
+
+		// ArrayList of class ItemsViewModel
+//		val data = ArrayList<ItemsViewModel>()
+
+
+		// This loop will create 20 Views containing
+		// the image with the count of view
+//		for (i in 1..20) {
+//			data.add(ItemsViewModel(R.drawable.ic_baseline_folder_24, "Item " + i))
+//		}
+
+
+
+
+	}
+
+	private fun getHtmlFromWeb() {
+		val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+		val data = GetCnbetaData()
+		// This will pass the ArrayList to our Adapter
+		val adapter = CustomAdapter(data)
+
+		// Setting the Adapter with the recyclerview
+		recyclerview.adapter = adapter
+
+	}
+
+	private fun GetCnbetaData(): List<ItemsViewModel> {
+		val data = ArrayList<ItemsViewModel>()
+
+		val url = "https://m.cnbeta.com.tw/"
+
+		val doc = Jsoup.connect(url).get()
+//		val html = doc.outerHtml()
+		val txtThumb = doc.getElementsByClass("txt_thumb")
+
+		var i=0
+		for (tt in txtThumb)
+		{
+			val img = tt.getElementsByTag("img")
+//			println(img.attr("src"))
+//			println(img.attr("alt"))
+			data.add(ItemsViewModel(R.drawable.ic_baseline_folder_24, i.toString() + img.attr("alt")))
+			i++
+		}
+
+		return data
+	}
+}
+
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	LearnRecycleviewTheme {
+		Greeting("Android")
+	}
+}
